a60cd83a98180b0557173cd2048c0706a4e806d9
Dmitriy <pd2018@yandex.ru>
2025-03-09 12:31:05 +0300

    Created HorseFullInfoDto

---
+++ OOP_Java/App/Core/DTOs/HorseFullInfoDto.java
@@ -0,0 +1,18 @@
+package OOP_Java.App.Core.DTOs;
+
+import java.util.LinkedList;
+
+import OOP_Java.App.Core.Enums.AnimalCommands;
+import OOP_Java.App.Core.Enums.AnimalKinds;
+import OOP_Java.App.Core.Enums.AnimalTypes;
+
+public record HorseFullInfoDto(
+                AnimalTypes animalType,
+                AnimalKinds petKind,
+                float weight,
+                int loadCapacity,
+                String name,
+                String birthDate,
+                LinkedList<AnimalCommands> commands,
+                int maxSpeed) {
+}
